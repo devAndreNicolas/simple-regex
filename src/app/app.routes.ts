1e9b25ede0f9bde17009cf3ae8c159d6aeb1d8b0
Andre Nicolas <devandrenicolas@gmail.com>
2025-10-01 21:34:26 -0300

    feat & style
    A politica de privacidade foi implementada, e o estilo do onboarding foi refatorado.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home'; 
 import { About } from './pages/about/about';
 import { Contact } from './pages/contact/contact';
+import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -9,6 +10,7 @@ export const routes: Routes = [
   {path: 'inicio', component: HomeComponent},
   { path: 'sobre', component: About },
   { path: 'contato', component: Contact },
+  { path: 'politica-de-privacidade', component: PrivacyPolicy},
   
   { path: '**', redirectTo: '' } 
 ];
